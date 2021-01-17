60eca0262076f1a52466895709073606374ce476
hadirga <halexandergc@gmail.com>
2021-01-17 13:09:18 -0300

    Change OrderSummary and Modal components to classes

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,39 +1,47 @@
-import React from "react";
+import React, { Component } from "react";
 
 import Container from '../../../hoc/Container';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients)
+class OrderSummary extends Component {
+    
+    componentWillUpdate() {
+        console.log('[OrderSummary] willUpdate');
+    }
+    
+    
+    render () {
+        const ingredientSummary = Object.keys(this.props.ingredients)
         .map(ingKey => {
             return (
                 <li key={ingKey}>
-                    <span style={{transform: 'capitalize'}}>{ingKey}</span>: {props.ingredients[ingKey]}
+                    <span style={{transform: 'capitalize'}}>{ingKey}</span>: {this.props.ingredients[ingKey]}
                 </li>
             );
         });
 
-    return (
-        <Container>
+        return (
+            <Container>
             <h3>Your Order</h3>
             <p>A delicious burger with the following ingredients:</p>
             <ul>
                 {ingredientSummary}
             </ul>
-            <p><b>Total Price: ${props.price.toFixed(2)}</b></p>
+            <p><b>Total Price: ${this.props.price.toFixed(2)}</b></p>
             <p>Continuous to Checkout?</p>
             <Button
                 btnType='Danger'
-                clicked={props.purchaseCancelled}
+                clicked={this.props.purchaseCancelled}
                 >CANCEL
             </Button>
             <Button
                 btnType='Success'
-                clicked={props.purchaseContinued}
+                clicked={this.props.purchaseContinued}
                 >CONTINUE
             </Button>
         </Container>
-    );
-};
+        );
+    }
+}
 
-export default orderSummary;
+export default OrderSummary;
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,24 +1,34 @@
-import React from 'react';
+import React, { Component } from "react";
 
-import classes from './Modal.css';
-import Contanier from '../../../hoc/Container';
-import Backdrop from '../Backdrop/Backdrop';
+import classes from "./Modal.css";
+import Contanier from "../../../hoc/Container";
+import Backdrop from "../Backdrop/Backdrop";
 
-const modal = (props) => (
-    <Contanier>
-        <Backdrop 
-            show={props.show} 
-            clicked={props.modalClosed}
-        />
-        <div 
-            className={classes.Modal}
-            style={{
-                transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                opacity: props.show ? '1' : '0'
-            }}
-            >{props.children}
+class Modal extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.show !== this.props.show;
+  }
+
+  componentWillUpdate() {
+    console.log("[Modal] willUpdate");
+  }
+
+  render() {
+    return (
+      <Contanier>
+        <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
+        <div
+          className={classes.Modal}
+          style={{
+            transform: this.props.show ? "translateY(0)" : "translateY(-100vh)",
+            opacity: this.props.show ? "1" : "0",
+          }}
+        >
+          {this.props.children}
         </div>
-    </Contanier>
-);
+      </Contanier>
+    );
+  }
+}
 
-export default modal;
+export default Modal;
